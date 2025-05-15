0f57c9396febb485988e445281a9c73993347ca1
guanshoukui <171930433@qq.com>
2025-05-15 15:26:25 +0800

    添加市内仓库场景

--- spot_bringup/launch/spot.gazebo.launch.py
+++ spot_bringup/launch/spot.gazebo.launch.py
@@ -14,10 +14,10 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    world_file = LaunchConfiguration('world_file', default='simple_tunnel.sdf')
+    world_file = LaunchConfiguration('world_file', default='tugbot_warehouse.sdf')
     world_file_arg = DeclareLaunchArgument(
         'world_file',
-        default_value='simple_tunnel.sdf',
+        default_value='tugbot_warehouse.sdf',
         description='Name of the world file to load'
     )
 
