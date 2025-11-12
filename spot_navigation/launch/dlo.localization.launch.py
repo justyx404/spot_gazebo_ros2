702d0f28b17ffe6d971b097d79677bf576016baf
Yixiang Gao <yixiangg310573@gmail.com>
2025-11-11 18:31:51 -0600

    localization basically done

--- spot_navigation/launch/dlo.localization.launch.py
+++ spot_navigation/launch/dlo.localization.launch.py
@@ -1,10 +1,11 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, RegisterEventHandler, Shutdown
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, Shutdown, IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
@@ -12,21 +13,21 @@ def generate_launch_description():
 
     rviz_arg = DeclareLaunchArgument(
         'rviz',
-        default_value='false', 
+        default_value='true', 
         description='Open RViz.'
-    )
-
-    rviz_config_file_arg = DeclareLaunchArgument(
-        'rviz_config_file',
-        default_value='dlo_localization.rviz',
-        description='RViz configuration file to use'
     )
 
     use_sim_time_arg = DeclareLaunchArgument(
 		'use_sim_time',
-		default_value='false',  # Set to 'true' for simulation
+		default_value='true',
 		description='Use simulation (Gazebo) clock if true'
 	)
+
+    map_path_arg = DeclareLaunchArgument(
+        'map_path',
+        default_value='simple_tunnel.pcd',
+        description='Filename of the map file (e.g., simple_tunnel.pcd).'
+    )
 
     # Visualize in RViz
     rviz = Node(
@@ -34,47 +35,29 @@ def generate_launch_description():
         executable='rviz2',
         arguments=['-d', PathJoinSubstitution([
             spot_nav_pkg, 
-            'config', 
-            LaunchConfiguration('rviz_config_file')
+            'rviz', 
+            'dlo_localization.rviz',
         ])],
         condition=IfCondition(LaunchConfiguration('rviz')),
         parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
 
-    pointcloud_topic_cfg = LaunchConfiguration('pointcloud_topic', default='/spot/lidar/points')
-    declare_pointcloud_topic_arg = DeclareLaunchArgument(
-    	'pointcloud_topic',
-    	default_value = pointcloud_topic_cfg,
-    	description = 'Input point cloud topic name'
-    )
-
-    imu_topic_cfg = LaunchConfiguration('imu_topic', default = 'dlo_imu_raw')
-    declare_imu_topic_arg = DeclareLaunchArgument(
-    	'imu_topic',
-    	default_value = imu_topic_cfg,
-    	description = 'Input IMU topic name'
+    dlo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                spot_nav_pkg,
+                'launch',
+                'dlo.launch.py'
+            ])
+        ),
+        launch_arguments={
+            'rviz': 'false',
+            'use_sim_time': LaunchConfiguration('use_sim_time')
+        }.items()
     )
 
     dlo_yaml_path = PathJoinSubstitution([spot_nav_pkg, 'config', 'dlo.yaml'])
-    dlo_odom_node = Node(
-    	name = 'dlo_odom',
-    	package = 'direct_lidar_odometry',
-    	executable = 'dlo_odom_node',
-    	output = 'screen',
-    	parameters = [
-			dlo_yaml_path,
-			{'use_sim_time': LaunchConfiguration('use_sim_time')}		
-		],
-    	remappings = [
-    		('imu'		 , imu_topic_cfg),
-    		('pointcloud', pointcloud_topic_cfg),
-			('submap'    , 'dlo/odom_node/submap'),
-    		('odom'      , 'dlo/odom_node/odom'),
-    		('pose'		 , 'dlo/odom_node/pose'),
-    		('kfs' 		 , 'dlo/odom_node/odom/keyframe'),
-    		('keyframe'	 , 'dlo/odom_node/pointcloud/keyframe')
-    	]
-    )
+    localization_yaml_path = PathJoinSubstitution([spot_nav_pkg, 'config', 'localization.yaml'])
 
     dlo_localization_node = Node(
         name = 'dlo_localization',
@@ -82,22 +65,19 @@ def generate_launch_description():
         executable = 'dlo_localization_node',
         output = 'screen',
         parameters = [
-            {'use_sim_time': LaunchConfiguration('use_sim_time')}
+            dlo_yaml_path,
+            localization_yaml_path,
+            {
+                'use_sim_time': LaunchConfiguration('use_sim_time'),
+                'dlo/localizationNode/map_path': PathJoinSubstitution([spot_nav_pkg, 'maps', LaunchConfiguration('map_path')])
+            }
         ],
         remappings= [
             ('global_map', 'dlo/localization_node/global_map'),
-            ('scan'      , 'dlo/odom_node/submap'),
+            ('pointcloud', '/dlo/odom_node/pointcloud/keyframe'),
             ('odom'      , 'dlo/odom_node/odom'),
-        ]       
+        ]
 	)
-    
-    # Event handlers to shutdown if either node exits
-    shutdown_on_odom_exit = RegisterEventHandler(
-        OnProcessExit(
-            target_action=dlo_odom_node,
-            on_exit=[Shutdown()]
-        )
-    )
 
     shutdown_on_localization_exit = RegisterEventHandler(
         OnProcessExit(
@@ -105,37 +85,13 @@ def generate_launch_description():
             on_exit=[Shutdown()]
         )
     )
-    # Use this for debugging purposes
-    static_map_to_odom_node = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_map_to_odom_publisher',
-        arguments=[
-            '--x', '0',
-            '--y', '0',
-            '--z', '0',
-            '--qx', '0',
-            '--qy', '0',
-            '--qz', '0',
-            '--qw', '1',
-            '--frame-id', 'map',
-            '--child-frame-id', 'odom'
-        ],
-        parameters = [
-            {'use_sim_time': LaunchConfiguration('use_sim_time')}
-        ],
-    )
 
     return LaunchDescription([
         rviz_arg,
-        rviz_config_file_arg,
         use_sim_time_arg,
+        map_path_arg,
         rviz,
-    	declare_pointcloud_topic_arg,
-    	declare_imu_topic_arg,
-    	dlo_odom_node,
+        dlo_launch,
         dlo_localization_node,
-        shutdown_on_odom_exit,
         shutdown_on_localization_exit
-        # static_map_to_odom_node,
     ])
